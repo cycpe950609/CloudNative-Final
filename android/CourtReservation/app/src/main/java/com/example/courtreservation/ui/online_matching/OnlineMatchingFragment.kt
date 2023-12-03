699995b7cb2b5d9d4a23db362dc551955d4835d0
yuhu-k <kyh08436.cs@gmail.com>
2023-12-03 23:46:29 +0800

    branch merge

--- android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/online_matching/OnlineMatchingFragment.kt
+++ android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/online_matching/OnlineMatchingFragment.kt
@@ -26,9 +26,10 @@ class OnlineMatchingFragment:Fragment() {
 
         _binding = FragmentOnlineMatchingBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        var bt1 = binding.findFriendBtn1
-        bt1.setOnClickListener {
-            bt1click(view)
+
+        val textView: TextView = binding.textOnlineMatching
+        onlineMatchingViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
         }
         return root
     }
@@ -36,37 +37,5 @@ class OnlineMatchingFragment:Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-    private fun bt1click(view: View?) {
-        val txtName: TextView = binding.txtName
-        val txtAge: TextView = binding.txtAge
-        val txtGender: TextView = binding.txtGender
-        val txtName1: TextView = binding.nameOut1
-        val txtName2: TextView = binding.nameOut2
-        val txtName3: TextView = binding.nameOut3
-        val txtName4: TextView = binding.ageOut4
-        val txtAge1: TextView = binding.ageOut1
-        val txtAge2: TextView = binding.ageOut2
-        val txtAge3: TextView = binding.ageOut3
-        val txtAge4: TextView = binding.ageOut4
-        val txtGender1: TextView = binding.genderOut1
-        val txtGender2: TextView = binding.genderOut2
-        val txtGender3: TextView = binding.genderOut3
-        val txtGender4: TextView = binding.genderOut4
-        txtName.visibility = View.VISIBLE
-        txtAge.visibility = View.VISIBLE
-        txtGender.visibility = View.VISIBLE
-        txtName1.visibility = View.VISIBLE
-        txtName2.visibility = View.VISIBLE
-        txtName3.visibility = View.VISIBLE
-        txtName4.visibility = View.VISIBLE
-        txtAge1.visibility = View.VISIBLE
-        txtAge2.visibility = View.VISIBLE
-        txtAge3.visibility = View.VISIBLE
-        txtAge4.visibility = View.VISIBLE
-        txtGender1.visibility = View.VISIBLE
-        txtGender2.visibility = View.VISIBLE
-        txtGender3.visibility = View.VISIBLE
-        txtGender4.visibility = View.VISIBLE
     }
 }
