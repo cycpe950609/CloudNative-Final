440fbd025f5a6d17e02e14245f1cee9d97ad9bd6
jingwei <a0979580915@gmail.com>
2023-12-07 22:44:53 +0800

    Finish login part, can connect to backend and function well

--- android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/forgot_password/ForgotPasswordActivity.kt
+++ android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/forgot_password/ForgotPasswordActivity.kt
@@ -1,11 +1,12 @@
 package com.example.courtreservation.ui.forgot_password
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.courtreservation.databinding.ActivityForgotPasswordBinding
-import com.example.courtreservation.ui.login.LoginActivity
+import com.example.courtreservation.network.PostDataTask
+import org.json.JSONObject
+import java.net.HttpURLConnection
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
@@ -19,19 +20,106 @@ class ForgotPasswordActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        binding.buttonSendCode.setOnClickListener {
+            send_code()
+        }
+
         binding.buttonChangePassword.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            change_password()
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                onBackPressed()
-                return true
+    private fun send_code() {
+        val email = binding.editTextForgotPasswordEmail.text.toString()
+        var user_type = "User"
+
+        val jsonBody = JSONObject()
+        jsonBody.put("email", email)
+        jsonBody.put("user_type", user_type)
+
+        val url = "https://cloudnative.eastasia.cloudapp.azure.com/login/send_reset_code"
+
+        PostDataTask { responseBody, responseCode ->
+            runOnUiThread {
+                if (responseBody.isNullOrEmpty()) {
+                    Toast.makeText(this, "No response from server", Toast.LENGTH_LONG).show()
+                    return@runOnUiThread
+                }
+
+                when (responseCode) {
+                    HttpURLConnection.HTTP_OK -> {
+                        // Handle success
+                        val message = JSONObject(responseBody).optString("message", "OK")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                    HttpURLConnection.HTTP_NOT_FOUND-> {
+                        // Handle not found
+                        val message = JSONObject(responseBody).optString("message", "Not Found")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                    else -> {
+                        // Handle other cases
+                        Toast.makeText(this, "Unexpected error: $responseCode", Toast.LENGTH_LONG).show()
+                    }
+                }
             }
+        }.execute(url, jsonBody.toString())
+    }
+
+    private fun change_password() {
+        val email = binding.editTextForgotPasswordEmail.text.toString()
+        var verificationCode = binding.editTextForgotPasswordCode.text.toString()
+        val newPassword = binding.editTextForgotPasswordNewPassword.text.toString()
+        val repeatNewPassword = binding.editTextForgotPasswordConfirmPassword.text.toString()
+        var user_type = "User"
+
+        if (newPassword != repeatNewPassword) {
+            Toast.makeText(this, "Password does not match", Toast.LENGTH_LONG).show()
+            return
         }
-        return super.onOptionsItemSelected(item)
+
+        if (!validatePassword(newPassword)) {
+            Toast.makeText(this, "Password must be at least 7 characters long and contains lowercase letter, number", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val jsonBody = JSONObject()
+        jsonBody.put("email", email)
+        jsonBody.put("reset_code", verificationCode)
+        jsonBody.put("new_password", newPassword)
+        jsonBody.put("user_type", user_type)
+
+        val url = "https://cloudnative.eastasia.cloudapp.azure.com/login/reset_password"
+
+        PostDataTask { responseBody, responseCode ->
+            runOnUiThread {
+                if (responseBody.isNullOrEmpty()) {
+                    Toast.makeText(this, "No response from server", Toast.LENGTH_LONG).show()
+                    return@runOnUiThread
+                }
+
+                when (responseCode) {
+                    HttpURLConnection.HTTP_OK -> {
+                        // Handle success
+                        val message = JSONObject(responseBody).optString("message", "OK")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                    HttpURLConnection.HTTP_BAD_REQUEST-> {
+                        // Handle bad request
+                        val message = JSONObject(responseBody).optString("message", "Bad request")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                    else -> {
+                        // Handle other cases
+                        Toast.makeText(this, "Unexpected error: $responseCode", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }.execute(url, jsonBody.toString())
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        val passwordRegex = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{7,}$".toRegex()
+        return passwordRegex.matches(password)
     }
 }
--- android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/register/RegisterActivity.kt
+++ android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/register/RegisterActivity.kt
@@ -1,11 +1,12 @@
 package com.example.courtreservation.ui.register
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.courtreservation.databinding.ActivityRegisterBinding
-import com.example.courtreservation.ui.login.LoginActivity
+import com.example.courtreservation.network.PostDataTask
+import org.json.JSONObject
+import java.net.HttpURLConnection
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -14,24 +15,89 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
-        supportActionBar?.hide()
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.buttonRegister.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            register()
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                onBackPressed()
-                return true
-            }
+    private fun register() {
+        val username = binding.editTextRegisterUsername.text.toString()
+        val password = binding.editTextRegisterPassword.text.toString()
+        val repeatpassword = binding.editTextRegisterRepeatPassword.text.toString()
+        val email = binding.editTextEmail.text.toString()
+        val user_type = "User"
+
+        if (username.isBlank()) {
+            Toast.makeText(this, "Username is required", Toast.LENGTH_LONG).show()
+            return
         }
-        return super.onOptionsItemSelected(item)
+
+        if (password.isBlank()) {
+            Toast.makeText(this, "Password is required", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (password != repeatpassword) {
+            Toast.makeText(this, "Password does not match", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (!validatePassword(password)) {
+            Toast.makeText(this, "Password must be at least 7 characters long and contains lowercase letter, number", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (!validateEmail(email)) {
+            Toast.makeText(this, "Invalid Email", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val jsonBody = JSONObject()
+        jsonBody.put("username", username)
+        jsonBody.put("password", password)
+        jsonBody.put("email", email)
+        jsonBody.put("user_type", user_type)
+
+        val url = "https://cloudnative.eastasia.cloudapp.azure.com/login/register"
+
+        PostDataTask { responseBody, responseCode ->
+            runOnUiThread {
+                if (responseBody.isNullOrEmpty()) {
+                    Toast.makeText(this, "No response from server", Toast.LENGTH_LONG).show()
+                    return@runOnUiThread
+                }
+
+                when (responseCode) {
+                    HttpURLConnection.HTTP_OK -> {
+                        // Handle success
+                        val message = JSONObject(responseBody).optString("message", "OK")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                    HttpURLConnection.HTTP_BAD_REQUEST-> {
+                        // Handle bad request
+                        val message = JSONObject(responseBody).optString("message", "Bad request")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                    else -> {
+                        // Handle other cases
+                        Toast.makeText(this, "Unexpected error: $responseCode", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }.execute(url, jsonBody.toString())
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        val passwordRegex = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{7,}$".toRegex()
+        return passwordRegex.matches(password)
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        val emailRegex = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$".toRegex()
+        return emailRegex.matches(email)
     }
 }
