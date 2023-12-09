851e7d8d06e1626ed782b8a8600c843f2565d04a
Ting Fang, Tsai <cycpe950609@gmail.com>
2023-12-10 02:11:53 +0800

    REMOVE: Remove clear cache header

--- backend/stadium/stadium.py
+++ backend/stadium/stadium.py
@@ -4,17 +4,17 @@ import sys
 
 app = Flask(__name__)
 
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+# @app.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     r.headers['Cache-Control'] = 'public, max-age=0'
+#     return r
 # Serve files
 root_dir = os.path.dirname(os.path.realpath(os.path.dirname(__file__)))
 PUBLIC_PATH = os.path.join(root_dir, "..", "public")
