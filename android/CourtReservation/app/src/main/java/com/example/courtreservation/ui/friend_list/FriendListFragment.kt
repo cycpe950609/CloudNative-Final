3a1830c99bb5811779e349b21e975dfdef717b73
Curtis114 <curtis890724@gmail.com>
2023-12-21 09:21:06 +0800

    add friend

--- android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/friend_list/FriendListFragment.kt
+++ android/CourtReservation/app/src/main/java/com/example/courtreservation/ui/friend_list/FriendListFragment.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.courtreservation.databinding.FragmentFriendListBinding
 import com.example.courtreservation.ui.gallery.GalleryViewModel
 import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
 class FriendsListFragment : Fragment() {
     private var _binding: FragmentFriendListBinding? = null
@@ -32,8 +39,33 @@ class FriendsListFragment : Fragment() {
     }
 
     private inner class FetchFriendsTask : AsyncTask<String, Void, String>() {
-        // Implement doInBackground and onPostExecute similar to FetchFriendRequestsTask
-        // In onPostExecute, call displayFriends instead of displayFriendRequests
+        override fun doInBackground(vararg urls: String): String? {
+            try {
+                val url = URL(urls[0])
+                with(url.openConnection() as HttpURLConnection) {
+                    requestMethod = "GET"
+                    return inputStream.bufferedReader().use { it.readText() }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                return null
+            }
+        }
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (result != null) {
+                try {
+                    val jsonResponse = JSONObject(result)
+                    val friends = jsonResponse.getJSONArray("friends")
+                    displayFriends(friends)
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                    Toast.makeText(context, "Error parsing response: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(context, "Network request failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun displayFriends(friends: JSONArray) {
@@ -49,9 +81,8 @@ class FriendsListFragment : Fragment() {
 
             val friendView = TextView(context).apply {
                 text = "Name: $userName, Age: $age, Gender: $gender, Height: $height"
-                layoutParams = linearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             }
-
             linearLayout.addView(friendView)
         }
     }
